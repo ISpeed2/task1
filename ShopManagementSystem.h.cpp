c2cab23c0a482b34716d39be9c76a70fa5966d3e
ISpeed2 <bro228nur@gmail.com>
2025-03-13 09:28:53 +0300

    Initial task0 commit

--- ShopManagementSystem.h.cpp
+++ ShopManagementSystem.h.cpp
@@ -2,22 +2,22 @@
 #define SHOPMANAGEMENTSYSTEM_H
 
 #include <vector>
-#include "Store.h" // Убедитесь, что путь к файлу Store.h правильный
+#include "Store.h"
 
 class ShopManagementSystem {
 private:
     std::vector<Store> stores;
 
 public:
-    ShopManagementSystem() = default; // Конструктор по умолчанию
+    ShopManagementSystem() = default;
 
-    // Методы для управления магазинами
     void add_store(const Store& store);
     void remove_store(int store_id);
-    Store* get_store(int store_id); // Возвращает указатель на магазин
+    Store* get_store(int store_id);
     std::vector<Store> get_all_stores() const;
 
-    // Симуляция
+    Store* find_store_with_lowest_price(int product_id);
+
     void run_simulation();
 };
 
