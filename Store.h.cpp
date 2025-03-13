03a34dcccf4a9e19d6c7b53e86cbae6f7a44003c
ISpeed2 <bro228nur@gmail.com>
2025-03-13 09:27:04 +0300

    Initial task0 commit

--- Store.h.cpp
+++ Store.h.cpp
@@ -3,21 +3,21 @@
 
 #include <string>
 #include <map>
-#include "StockItem.h" // Включаем StockItem!
+#include "StockItem.h"
 
 class Store {
 private:
     std::string name;
     int id;
     std::string address;
-    std::map<int, StockItem> inventory; // Используем StockItem!
+    std::map<int, StockItem> inventory;
 
 public:
     Store(std::string name, int id, std::string address);
+
     std::string get_name() const;
     int get_id() const;
     std::string get_address() const;
-
     std::map<int, StockItem>& get_inventory(); // Возвращаем ссылку!
 
     void add_product(int product_id, const Product& product, int quantity);
