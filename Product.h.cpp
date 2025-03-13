17ef9480bbb7fa53f411c8e84d5cdb4959053cc5
ISpeed2 <bro228nur@gmail.com>
2025-03-13 09:26:14 +0300

    Initial task0 commit

--- Product.h.cpp
+++ Product.h.cpp
@@ -2,25 +2,19 @@
 #define PRODUCT_H
 
 #include <string>
-#include <iostream>
 
 class Product {
 private:
     std::string name;
     std::string description;
-    int id;
     double price;
 
 public:
-    Product(std::string name, std::string description, int id, double price);
+    Product(std::string name, std::string description, double price);
 
     std::string get_name() const;
     std::string get_description() const;
-    int get_id() const;
     double get_price() const;
-    void set_price(double price);
-
-    void print_info() const;
 };
 
 #endif
