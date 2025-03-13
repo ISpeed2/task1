b9e2db40ffa6d8f502e9bde3beb0a3924984d9d3
ISpeed2 <bro228nur@gmail.com>
2025-03-13 09:22:43 +0300

    Initial task0 commit

--- Customer.h.cpp
+++ Customer.h.cpp
@@ -1,32 +1,28 @@
-﻿#ifndef CUSTOMER_H
+#ifndef CUSTOMER_H
 #define CUSTOMER_H
 
-#include <iostream>
 #include <string>
 #include <map>
-#include "Store.h" // Подключаем Store.h
 
 class Customer {
 private:
     std::string name;
-    int id;
     double balance;
     std::map<int, int> shopping_list;
 
 public:
-    Customer(std::string name, double balance, int id);
+    Customer(std::string name, double balance); // Изменен конструктор!
 
     std::string get_name() const;
-    int get_id() const;
     double get_balance() const;
+    void set_balance(double balance);
+    std::map<int, int> get_shopping_list() const;
 
     void add_to_shopping_list(int product_id, int quantity);
     void remove_from_shopping_list(int product_id);
-    bool buy_products(Store& store);
-    void deposit(double amount);
-    void withdraw(double amount);
+    void make_purchase(double amount);
 
-    void print_shopping_list() const;
+    void print_info() const; // Функция для вывода информации
 };
 
 #endif
